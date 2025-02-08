f326d4277c68d87185ee5d43fa10d643195ec1c3
Timothy Warren Wooten <166267132+wrekk777@users.noreply.github.com>
2025-02-08 09:58:03 -0500

    Update and rename script_b to scripts_b.js

--- static/js/scripts_b.js
+++ static/js/scripts_b.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home','projects','resume','wayfarer']
+const section_names = ['bio']
 
 
 window.addEventListener('DOMContentLoaded', event => {
